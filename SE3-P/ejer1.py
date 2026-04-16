2045497e7ecf66a1947f69a97673deebf9de3fdc
Punchks <jhordylucascalla@gmail.com>
2026-04-16 15:10:33 -0500

    subiendo ejer 1 en py

---
+++ SE3-P/ejer1.py
@@ -0,0 +1,8 @@
+nombre=input("Ingrese nombre:")
+carrera=input("Ingrese carrera:")
+
+print("\n-----IMPRESO CON + -----")
+print(nombre + ",Bienvenido al curso de fundamentos de algoritmos de la carrera" + carrera) 
+
+print("\n-----IMPRESO CON f -----")
+print(f"{nombre},Bienvenido al curso de fundamentos de algoritmos de la carrera{carrera}")
